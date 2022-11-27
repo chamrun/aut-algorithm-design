e43cd5fa3ad2efaccb5c7ede4598d5df4a8104b8
ChamRun <chrm@aut.ac.ir>
2022-11-27 13:49:10 +0330

    finish `convertArrayToBST` function

--- hw1/quera/main.go
+++ hw1/quera/main.go
@@ -2,6 +2,50 @@ package main
 
 import "fmt"
 
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+func convertArrayToBST(arr []int) *Node {
+	if len(arr) == 0 {
+		return nil
+	}
+
+	mid := len(arr) / 2
+	root := &Node{arr[mid], nil, nil}
+	root.left = convertArrayToBST(arr[:mid])
+	root.right = convertArrayToBST(arr[mid+1:])
+	return root
+}
+
+func beatifyBST(root *Node) {
+	if root == nil {
+		return
+	}
+
+	beatifyBST(root.left)
+	fmt.Println(root.value)
+	beatifyBST(root.right)
+}
+
 func main() {
-	fmt.Println("hello world")
+	numbers := []int{
+		-5,
+		-10,
+		0,
+		15,
+		20,
+		100,
+		-100,
+	}
+
+	//input := getInputUntilEOF()
+	//fmt.Println(input)
+
+	bst := convertArrayToBST(numbers)
+
+	beatifyBST(bst)
+
 }
