c9f0b26a23f874d06b1fec94b1aab2ecf7e7b211
ChamRun <chrm@aut.ac.ir>
2022-12-30 18:17:04 +0330

    Update q3.py

--- hw2/quera/python/q3.py
+++ hw2/quera/python/q3.py
@@ -1,40 +1,6 @@
-def meets_conditions(conditions, a, b, c, d):
-    for condition in conditions['odd']:
-        sum_of_acts = a
-
-        if 'b' in condition:
-            sum_of_acts += b
-        elif 'c' in condition:
-            sum_of_acts += c
-
-        if 'd' in condition:
-            sum_of_acts += d
-
-        if sum_of_acts % 2 == 0:
-            return False
-
-    for condition in conditions['even']:
-        sum_of_acts = a
-
-        if 'b' in condition:
-            sum_of_acts += b
-        elif 'c' in condition:
-            sum_of_acts += c
-
-        if 'd' in condition:
-            sum_of_acts += d
-
-        if sum_of_acts % 2 == 1:
-            return False
-
-    return True
-
-
 def solve(n, abcd, ons, offs):
-    conditions = {
-        'odd': list(),
-        'even': list(),
-    }
+    should_be_odd = {}
+
     for i in range(n):
         if i in ons:
             i_changed = False
@@ -43,61 +9,131 @@ def solve(n, abcd, ons, offs):
         else:
             continue
 
-        clicks_by_b = i % 2 == 0
-        clicks_by_c = not clicks_by_b
-        clicks_by_d = i % 3 == 0
+        by_six = i % 6
 
-        clicks_by = set()
-
-        if clicks_by_b:
-            clicks_by.add('b')
-        if clicks_by_c:
-            clicks_by.add('c')
-        if clicks_by_d:
-            clicks_by.add('d')
-
-        if i_changed:
-            conditions['odd'].append(clicks_by)
+        if by_six == 0:
+            if 'bd' in should_be_odd and should_be_odd['bd'] != i_changed:
+                return []
+            should_be_odd['bd'] = i_changed
+        elif by_six == 1 or by_six == 5:
+            if 'c' in should_be_odd and should_be_odd['c'] != i_changed:
+                return []
+            should_be_odd['c'] = i_changed
+        elif by_six == 2 or by_six == 4:
+            if 'b' in should_be_odd and should_be_odd['b'] != i_changed:
+                return []
+            should_be_odd['b'] = i_changed
         else:
-            conditions['even'].append(clicks_by)
+            if 'cd' in should_be_odd and should_be_odd['cd'] != i_changed:
+                return []
+            should_be_odd['cd'] = i_changed
 
     possible_states = set()
+
+    ab_should_be_odd = should_be_odd.get('b')
+    ac_should_be_odd = should_be_odd.get('c')
+    abd_should_be_odd = should_be_odd.get('bd')
+    acd_should_be_odd = should_be_odd.get('cd')
+
     for a in range(abcd + 1):
-        for b in range(abcd - a + 1):
-            for c in range(abcd - a - b + 1):
-                d = abcd - a - b - c
 
-                if meets_conditions(conditions, a, b, c, d):
+        if ab_should_be_odd is not None:
+            b_steps = 2
+            if a % 2 == 0:
+                if ab_should_be_odd:
+                    b_start = 1
+                else:
+                    b_start = 0
+            else:
+                if ab_should_be_odd:
+                    b_start = 0
+                else:
+                    b_start = 1
+        else:
+            b_start = 0
+            b_steps = 1
 
-                    state = ''
+        if ac_should_be_odd is not None:
+            c_steps = 2
+            if a % 2 == 0:
+                if ac_should_be_odd:
+                    c_start = 1
+                else:
+                    c_start = 0
+            else:
+                if ac_should_be_odd:
+                    c_start = 0
+                else:
+                    c_start = 1
+        else:
+            c_start = 0
+            c_steps = 1
 
-                    for i in range(n):
-                        if i in ons:
-                            state += '1'
-                        elif i in offs:
-                            state += '0'
+        for b in range(b_start, abcd - a + 1, b_steps):
+
+            if abd_should_be_odd is not None:
+                d_steps = 2
+                if (a + b) % 2 == 0:
+                    if abd_should_be_odd:
+                        d_start = 1
+                    else:
+                        d_start = 0
+                else:
+                    if abd_should_be_odd:
+                        d_start = 0
+                    else:
+                        d_start = 1
+            else:
+                d_start = 0
+                d_steps = 1
+
+            for d in range(d_start, abcd - a - b + 1, d_steps):
+                c = abcd - a - b - d
+
+            # for c in range(c_start, abcd - a - b + 1, c_steps):
+            #     d = abcd - a - b - c
+
+                if abd_should_be_odd is not None:
+                    abd_is_odd = (a + b + d) % 2 == 1
+                    if abd_is_odd != abd_should_be_odd:
+                        continue
+
+                if acd_should_be_odd is not None:
+                    acd_is_odd = (a + b + c) % 2 == 1
+                    if acd_is_odd != acd_should_be_odd:
+                        continue
+
+                state = ''
+
+                for i in range(n):
+                    if i in ons:
+                        state += '1'
+                    elif i in offs:
+                        state += '0'
+                    else:
+                        clicks_by_b = i % 2 == 0
+                        clicks_by_d = i % 3 == 0
+
+                        total_clicks = a
+
+                        if clicks_by_b:
+                            total_clicks += b
                         else:
-                            clicks_by_b = i % 2 == 0
-                            clicks_by_c = not clicks_by_b
-                            clicks_by_d = i % 3 == 0
+                            total_clicks += c
 
-                            total_clicks = a
+                        if clicks_by_d:
+                            total_clicks += d
 
-                            if clicks_by_b:
-                                total_clicks += b
-                            elif clicks_by_c:
-                                total_clicks += c
-                            if clicks_by_d:
-                                total_clicks += d
+                        i_is_on = total_clicks % 2 == 0
 
-                            i_is_on = total_clicks % 2 == 0
+                        if i_is_on:
+                            state += '1'
+                        else:
+                            state += '0'
+                possible_states.add(state)
 
-                            if i_is_on:
-                                state += '1'
-                            else:
-                                state += '0'
-
-                    possible_states.add(state)
+    possible_states = list(possible_states)
+    possible_states.sort()
     return possible_states
 
 
@@ -106,7 +142,6 @@ def print_answer(ans):
         print('IMPOSSIBLE')
         return
     ans_list = list(ans)
-    ans_list.sort()
     for s in ans_list:
         print(s)
 
